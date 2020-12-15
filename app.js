5a993ec1f42c23412ea7ab97ac5d53e14d30d0c9
heyprashant <prashant.p7804@gmail.com>
2020-12-15 20:28:47 +0530

    Add note adding functionality

--- app.js
+++ app.js
@@ -1,8 +1,58 @@
-const chalk = require('chalk')
-const getNotes = require('./notes')
+const chalk = require('chalk');
+const yargs = require('yargs');
+const notes = require('./notes');
 
+//Customize yargs version
+yargs.version('1.1.0');
 
-const notes = getNotes();
-console.log(notes)
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder:{
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body);
+    }
+})
 
-console.log(chalk.green('Success' , chalk.red("i Am very bold") + chalk.inverse(' Red inverse')))
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing a note!')
+    }
+})
+
+//Create list command 
+yargs.command({
+    command: 'list',
+    describe: 'List the notes',
+    handler: function () {
+        console.log("Listing out all notes!")
+    }
+})
+
+//Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Reads a note',
+    handler: function () {
+        console.log('Reading a note!')
+    }
+})
+
+yargs.parse();
+//console.log(yargs.argv);
+
--- notes.js
+++ notes.js
@@ -1,5 +1,47 @@
+const { notDeepEqual } = require('assert');
+const fs = require('fs');
+
 const getNotes = function() {
     return "Your notes..."
 }
 
-module.exports = getNotes
+const addNote = function(title, body) {
+    const notes = loadNotes();
+    const duplicateNotes = notes.filter( function (note) {
+        return note.title === title;
+    });
+    
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+    
+        saveNotes(notes);
+        console.log('new note added!');
+    } else {
+        console.log('Note title taken!');
+    }
+}
+
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
+}
+
+//first time when there is no file, this indeed throw an error and return an empty array from catch block
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (e) {
+         return [];   
+    }
+    
+}
+
+module.exports = {
+    getNotes : getNotes,
+    addNote: addNote
+}
